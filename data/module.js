e40792be06570da5e770e0d420717bd4521efe84
Joel <joelchirit96@gmail.com>
2023-04-11 17:38:35 -0500

    Creando un nuevo dataset modularizado sobre cada periodo presidencial con sus respectivos datos de inflacion e identificadores

---
+++ data/module.js
@@ -0,0 +1,74 @@
+const MODULE = [
+    {
+        "ID":"Primer gobierno de alan garcía",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [1987, 1988, 1989, 1990],
+        "HUELGAS_PLIEGO_RECLAMOS": [222, 267, 202, 200],
+        "HUELGAS_OTRAS_CAUSAS": [498, 547, 465, 413],
+        "INFLATION": [85.8223062381847, 667.019328585961, 3398.67901005332, 7481.66361124526]
+    },
+    {
+        "ID": "Gobierno de Alberto Fujimori",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000],
+        "HUELGAS_PLIEGO_RECLAMOS": [200, 126, 97, 106, 68, 33, 28, 21, 13, 9, 15],
+        "HUELGAS_OTRAS_CAUSAS": [413, 189, 122, 45, 100, 69, 49, 45, 45, 62, 22],
+        "INFLATION": []
+    },
+    {
+        "ID": "Gobierno de Valentin Paniagua",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2000, 2001],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    },
+    {
+        "ID": "Gobierno de Alejandro Toledo",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2001, 2002, 2003, 2004, 2005, 2006],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    },
+    {
+        "ID": "Segundo gobierno de Alan García",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2006, 2007, 2008, 2009, 2010, 2011],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    },
+    {
+        "ID": "Gobierno de Ollanta Humala",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2011, 2012, 2013, 2014, 2015, 2016],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    },
+    {
+        "ID": "Gobierno de PPK",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2016, 2017, 2018],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    },
+    {
+        "ID": "Gobierno de Martín Vizcarra",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2018, 2019, 2020],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    },
+    {
+        "ID": "Gobierno de Francisco Sagasti",
+        "FECHA_CORTE": 20230330,
+        "ANIOS": [2020, 2021],
+        "HUELGAS_PLIEGO_RECLAMOS": [],
+        "HUELGAS_OTRAS_CAUSAS": [],
+        "INFLATION": []
+    }
+]
