3ff19ce4130474ea4f9ba8e44751f58834dc603a
Joel <joelchirit96@gmail.com>
2023-04-12 11:29:45 -0500

    Modularizamos los datos de cada periodo presidencial en un archivo con un array de objetos. Ojalá funcione xd

--- data/module.js
+++ data/module.js
@@ -5,7 +5,13 @@ const MODULE = [
         "ANIOS": [1987, 1988, 1989, 1990],
         "HUELGAS_PLIEGO_RECLAMOS": [222, 267, 202, 200],
         "HUELGAS_OTRAS_CAUSAS": [498, 547, 465, 413],
-        "INFLATION": [85.8223062381847, 667.019328585961, 3398.67901005332, 7481.66361124526]
+        "INFLATION": 
+            [
+                85.8223062381847, 
+                667.019328585961, 
+                3398.67901005332, 
+                7481.66361124526
+            ]
     },
     {
         "ID": "Gobierno de Alberto Fujimori",
@@ -13,62 +19,117 @@ const MODULE = [
         "ANIOS": [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000],
         "HUELGAS_PLIEGO_RECLAMOS": [200, 126, 97, 106, 68, 33, 28, 21, 13, 9, 15],
         "HUELGAS_OTRAS_CAUSAS": [413, 189, 122, 45, 100, 69, 49, 45, 45, 62, 22],
-        "INFLATION": []
+        "INFLATION": 
+            [
+                7481.66361124526, 
+                409.530166622023, 
+                73.5282942442641, 
+                48.5799864468884, 
+                23.7369050565046, 
+                11.1287029707088, 
+                11.5379424115177, 
+                8.56218948953654, 
+                7.24782175213469, 
+                3.46965996293021, 
+                3.75733833344474
+            ]
     },
     {
         "ID": "Gobierno de Valentin Paniagua",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2000, 2001],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [15, 8],
+        "HUELGAS_OTRAS_CAUSAS": [22, 32],
+        "INFLATION": 
+            [
+                3.75733833344474, 
+                1.97709878502052
+            ]
     },
     {
         "ID": "Gobierno de Alejandro Toledo",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2001, 2002, 2003, 2004, 2005, 2006],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [8, 12, 13, 12, 20, 18],
+        "HUELGAS_OTRAS_CAUSAS": [32, 52, 55, 95, 45, 49],
+        "INFLATION": 
+            [
+                1.97709878502052, 
+                0.193135003853723, 
+                2.25937753363075, 
+                3.66247320092231, 
+                1.61630193862544, 
+                2.00225801248821
+            ]
     },
     {
         "ID": "Segundo gobierno de Alan García",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2006, 2007, 2008, 2009, 2010, 2011],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [18, 20, 20, 40, 32, 36],
+        "HUELGAS_OTRAS_CAUSAS": [49, 53, 43, 59, 51, 48],
+        "INFLATION": 
+            [
+                2.00225801248821, 
+                1.77998644680372, 
+                5.78587598206754, 
+                2.93623153596699, 
+                1.52832059732903, 
+                3.36931095333853
+            ]
     },
     {
         "ID": "Gobierno de Ollanta Humala",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2011, 2012, 2013, 2014, 2015, 2016],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [36, 33, 44, 45, 25, 22],
+        "HUELGAS_OTRAS_CAUSAS": [48, 56, 50, 50, 22, 19],
+        "INFLATION": 
+            [
+                3.36931095333853, 
+                3.61121294940175, 
+                2.76789666081994, 
+                3.4119457980747, 
+                3.39809194746299, 
+                3.55717663556281
+            ]
     },
     {
         "ID": "Gobierno de PPK",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2016, 2017, 2018],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [22, 17, 26],
+        "HUELGAS_OTRAS_CAUSAS": [19, 28, 28],
+        "INFLATION": 
+            [
+                3.55717663556281, 
+                2.99490045809446, 
+                1.50915423024742
+            ]
     },
     {
         "ID": "Gobierno de Martín Vizcarra",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2018, 2019, 2020],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [26, 27, 5],
+        "HUELGAS_OTRAS_CAUSAS": [28, 40, 18],
+        "INFLATION": 
+            [
+                1.50915423024742, 
+                2.25212191358024, 
+                2.00241205759293
+            ]
     },
     {
         "ID": "Gobierno de Francisco Sagasti",
         "FECHA_CORTE": 20230330,
         "ANIOS": [2020, 2021],
-        "HUELGAS_PLIEGO_RECLAMOS": [],
-        "HUELGAS_OTRAS_CAUSAS": [],
-        "INFLATION": []
+        "HUELGAS_PLIEGO_RECLAMOS": [25, 23],
+        "HUELGAS_OTRAS_CAUSAS": [13, 18],
+        "INFLATION": 
+            [
+                2.00241205759293, 
+                4.27166381536796, 
+            ]
     }
 ]
