ebf6b4ceab8e02f36b61ef3f4e59b1e4d208e2c4
Joel <joelchirit96@gmail.com>
2023-04-12 16:34:47 -0500

    Aplicacion 1.0 con renderizado de charts correcta

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 import { chartGeneral } from './components/index.js'
 import { dateStrikes } from '../data/strikes.js'
 import { dateAlanFirst } from '../data/alanFirst.js'
-import { dateALanTwo } from '../data/alanTwo.js'
+import { dateAlanTwo } from '../data/alanTwo.js'
+import { dateFujimori } from '../data/fujimori.js'
+import { dateOllanta } from '../data/ollanta.js'
+import { datePaniagua } from '../data/paniagua.js'
+import { datePPK } from '../data/ppk.js'
+import { dateSagasti } from '../data/sagasti.js'
+import { dateToledo } from '../data/toledo.js'
+import { dateVizcarra } from '../data/vizcarra.js'
 
+const ctxGeneral = document.querySelector('#general')
 const ctxAlan1 = document.querySelector('#alan1')
+const ctxFujimori = document.querySelector('#fujimori')
+const ctxPaniagua = document.querySelector('#paniagua')
+const ctxToledo = document.querySelector('#toledo')
 const ctxAlan2 = document.querySelector('#alan2')
+const ctxOllanta = document.querySelector('#ollanta')
+const ctxPPK = document.querySelector('#ppk')
+const ctxVizcarra = document.querySelector('#vizcarra')
+const ctxSagasti = document.querySelector('#sagasti')
 
-chartGeneral(ctxAlan1, dateStrikes, dateStrikes[0].ID)
-chartGeneral(ctxAlan2, dateAlanFirst, dateAlanFirst[0].ID)
+chartGeneral(ctxGeneral, dateStrikes)
+chartGeneral(ctxAlan1, dateAlanFirst)
+chartGeneral(ctxFujimori, dateFujimori)
+chartGeneral(ctxPaniagua, datePaniagua)
+chartGeneral(ctxToledo, dateToledo)
+chartGeneral(ctxAlan2, dateAlanTwo)
+chartGeneral(ctxOllanta, dateOllanta)
+chartGeneral(ctxPPK, datePPK)
+chartGeneral(ctxVizcarra, dateVizcarra)
+chartGeneral(ctxSagasti, dateSagasti)
