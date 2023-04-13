34c2bb2e6a1143f5eb10d3a66dce4b555f3f05a2
Joel <joelchirit96@gmail.com>
2023-04-13 16:15:48 -0500

    Archivos de la carpeta components han sido modularizados

--- src/components/index.js
+++ src/components/index.js
@@ -1,30 +1,9 @@
 import { data } from './dataSets.js'
+import { dataOptions } from './dataOptions.js'
 
 export const chartGeneral = (ctx, option, title) => new Chart(ctx, {
 
     type: 'line',
     data: data(option),
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                text: title,
-                padding: {
-                    top: 30,
-                    bottom: 10
-                }
-            }
-        },
-        scales: {
-            y: {
-                beginAtZero: true,
-                position: 'left'
-            },
-            y1: {
-                beginAtZero: true,
-                position: 'right'
-            }
-        },
-        responsive: true
-    }
+    options: dataOptions(title)
 })
