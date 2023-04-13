87c68a321d3813a124f3349e5db0aa5dd890c524
Joel <joelchirit96@gmail.com>
2023-04-13 17:00:04 -0500

    Grafico de linea

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import { dataOptions } from './dataOptions.js'
 
 export const chartGeneral = (ctx, option, title) => new Chart(ctx, {
 
-    type: 'bar',
+    type: 'line',
     data: data(option),
     options: dataOptions(title)
 })
