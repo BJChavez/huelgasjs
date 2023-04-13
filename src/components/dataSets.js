c004d80546e26eecef1c570c45d065439584adef
Joel <joelchirit96@gmail.com>
2023-04-13 17:19:14 -0500

    Se agrego estilos a las lineas de los graficos

--- src/components/dataSets.js
+++ src/components/dataSets.js
@@ -8,6 +8,8 @@ export const data = (option) => {
                 data: option.map(p => p.HUELGAS_PLIEGO_RECLAMOS),
                 backgroundColor: '#005f73',
                 borderWidth: 1,
+                fill: false,
+                tension: 0.4,
                 borderColor: '#005f73',
                 yAxisID: 'y'
             },
@@ -16,6 +18,8 @@ export const data = (option) => {
                 data: option.map(c => parseInt(Math.round(c.INFLATION))),
                 backgroundColor: '#c1121f',
                 borderWidth: 1,
+                fill: false,
+                tension: 0.4,
                 borderColor: '#c1121f',
                 yAxisID: 'y1'
             }
