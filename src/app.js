8f97f95bc8a90b16fc1d8d28b12f7e6988a5d523
Joel <joelchirit96@gmail.com>
2023-04-13 18:57:51 -0500

    Resolviendo el problema de crear nodos interactivos con JavaScript

--- src/app.js
+++ src/app.js
@@ -1,27 +1,53 @@
 import { chartGeneral } from './components/index.js'
-//import { dataStrikes } from '../data/strikes.js'
-
 import {dataAlanFirst, dataAlanTwo, dataFujimori, dataOllanta, dataPPK, dataPaniagua, dataSagasti, dataToledo, dataVizcarra} from '../data/presidents.js'
 
 
-//const ctxGeneral = document.querySelector('#general')
-const ctxAlan1 = document.querySelector('#alan1').getContext('2d')
-const ctxFujimori = document.querySelector('#fujimori').getContext('2d')
-const ctxPaniagua = document.querySelector('#paniagua').getContext('2d')
-const ctxToledo = document.querySelector('#toledo').getContext('2d')
-const ctxAlan2 = document.querySelector('#alan2').getContext('2d')
-const ctxOllanta = document.querySelector('#ollanta').getContext('2d')
-const ctxPPK = document.querySelector('#ppk').getContext('2d')
-const ctxVizcarra = document.querySelector('#vizcarra').getContext('2d')
-const ctxSagasti = document.querySelector('#sagasti').getContext('2d')
+//El problema es los id
+//const ctxAlan1 = document.querySelector('#alan1').getContext('2d')
+//const ctxFujimori = document.querySelector('#fujimori').getContext('2d')
+//const ctxPaniagua = document.querySelector('#paniagua').getContext('2d')
+//const ctxToledo = document.querySelector('#toledo').getContext('2d')
+//const ctxAlan2 = document.querySelector('#alan2').getContext('2d')
+//const ctxOllanta = document.querySelector('#ollanta').getContext('2d')
+//const ctxPPK = document.querySelector('#ppk').getContext('2d')
+//const ctxVizcarra = document.querySelector('#vizcarra').getContext('2d')
+//const ctxSagasti = document.querySelector('#sagasti').getContext('2d')
 
-//chartGeneral(ctxGeneral, dataStrikes)
-chartGeneral(ctxAlan1, dataAlanFirst.options, dataAlanFirst.title)
-chartGeneral(ctxFujimori, dataFujimori.options, dataFujimori.title)
-chartGeneral(ctxPaniagua, dataPaniagua.options, dataPaniagua.title)
-chartGeneral(ctxToledo, dataToledo.options, dataToledo.title)
-chartGeneral(ctxAlan2, dataAlanTwo.options, dataAlanTwo.title)
-chartGeneral(ctxOllanta, dataOllanta.options, dataOllanta.title)
-chartGeneral(ctxPPK, dataPPK.options, dataPPK.title)
-chartGeneral(ctxVizcarra, dataVizcarra.options, dataVizcarra.title)
-chartGeneral(ctxSagasti, dataSagasti.options, dataSagasti.title)
+//const arraysCtx = [ctxAlan1, ctxFujimori, ctxPaniagua, ctxToledo, ctxAlan2, ctxOllanta, ctxPPK, ctxVizcarra, ctxSagasti]
+const arraysData = [dataAlanFirst, dataAlanTwo, dataFujimori, dataOllanta, dataPPK, dataPaniagua, dataSagasti, dataToledo, dataVizcarra]
+
+console.log(arraysData)
+
+const containerG = document.querySelector('#containerG')
+
+function createForm(){
+
+    for(let i = 0; i <= arraysData.length; i++){
+
+        chartGeneral(i, i.options, i.ide)
+
+        //const newDiv = document.createElement('div')
+        //newDiv.className = 'row'
+//
+        //const newCanvas = document.createElement('canvas')
+        //newCanvas.setAttribute('id', i.ide)
+//
+        //containerG.append(newDiv)
+        //newDiv.append(newCanvas)
+//
+        //chartGeneral(i, i.options, i.title)
+    }
+}
+
+createForm()
+
+
+//chartGeneral(ctxAlan1, dataAlanFirst.options, dataAlanFirst.title)
+//chartGeneral(ctxFujimori, dataFujimori.options, dataFujimori.title)
+//chartGeneral(ctxPaniagua, dataPaniagua.options, dataPaniagua.title)
+//chartGeneral(ctxToledo, dataToledo.options, dataToledo.title)
+//chartGeneral(ctxAlan2, dataAlanTwo.options, dataAlanTwo.title)
+//chartGeneral(ctxOllanta, dataOllanta.options, dataOllanta.title)
+//chartGeneral(ctxPPK, dataPPK.options, dataPPK.title)
+//chartGeneral(ctxVizcarra, dataVizcarra.options, dataVizcarra.title)
+//chartGeneral(ctxSagasti, dataSagasti.options, dataSagasti.title)
