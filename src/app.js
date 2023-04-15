1d109abfee7caf7e3ec594f66110269f13002cb8
Joel <joelchirit96@gmail.com>
2023-04-15 18:35:05 -0500

    Estilos mejorados de la imagen el titulo y los graficos. Ademas ahora tailwind escucha app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,18 @@ const containerGeneral = document.querySelector('#containerG')
 
 function createChart(){
     arraysData.forEach(e => {
-        const newDiv = document.createElement('div')
-        //newDiv.classList.add('','')
 
+        const newDiv = document.createElement('div')
         const newCanvas = document.createElement('canvas')
+        
+        newDiv.classList.add('p-12')
+        newCanvas.classList.add('p-3', 
+        'border-2', 
+        'border-teal-600', 
+        'rounded-md',
+        'bg-white')
+
         newCanvas.setAttribute('id', e.id)
-        //newCancas.classList('','')
 
         newDiv.append(newCanvas)
         containerGeneral.append(newDiv)
