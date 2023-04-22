e22308c262343c884e84a6216ff76c804b6d63c3
Joel <joelchirit96@gmail.com>
2023-04-22 12:19:18 -0500

    Se movieron algunos archivos para que sea mas entendible la estructura del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html","./src/app.js"],
+  content: ["./index.html","./src/app.js"],
   theme: {
     fontFamily:{
       'bebas':['Bebas Neue']
