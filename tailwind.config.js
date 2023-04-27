19362b96b526a59071e71ae8241a2392c980c469
BJChavez <joelchirit96@gmail.com>
2023-04-27 16:01:16 -0500

    Se intercambiaron los colores del boton de descarga

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
       head: 'rgb(0, 43, 91)',
       text: 'rgb(249, 226, 175)',
       textSub: 'rgb(234, 84, 85)',
-      bgButtom: 'rgb(4, 167, 119)',
-      hoverButtom: 'rgb(4, 167, 119, .8)',
+      bgButtom: 'rgb(4, 167, 119, .8)',
+      hoverButtom: 'rgb(4, 167, 119)',
       textButtom: 'rgb(255, 255, 255)',
       canva: 'rgb(249, 226, 175, .1)',
       borde: 'rgb(0, 43, 91)'
