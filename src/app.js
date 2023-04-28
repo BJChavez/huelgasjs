b541882ad4e8965f2dbc9692b664e2c1713e1e3f
BJChavez <joelchirit96@gmail.com>
2023-04-28 00:31:31 -0500

    Codigo de las clases del canvas separado en un archivo en la carpeta helpers

--- src/app.js
+++ src/app.js
@@ -1,24 +1,14 @@
 import { chartGeneral } from './components/graphic.js'
+import { CANVAS_CLASS } from './helpers/styles.canvas.js'
 import { dataPresidents } from '../data/db.js'
 import { changeTypeChart } from './events/event.js'
 
 const containerGeneral = document.querySelector('#chart')
-const newCanvasClassList = [
-  'lg:m-20',
-  'md:m-10',
-  'm-2',
-  'border-2',
-  'border-dashed',
-  'border-borde',
-  'rounded-lg',
-  'p-3',
-  'bg-canva'
-]
 
 function createChart () {
   dataPresidents.map(data => {
     const newCanvas = document.createElement('canvas')
-    newCanvas.classList.add(...newCanvasClassList)
+    newCanvas.classList.add(...CANVAS_CLASS)
     newCanvas.setAttribute('id', data.id)
 
     containerGeneral.append(newCanvas)
