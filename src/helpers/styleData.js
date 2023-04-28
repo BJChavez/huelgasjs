b00b1a233288cae0e7a0cc4b4713cb25a003db3a
BJChavez <joelchirit96@gmail.com>
2023-04-28 13:34:42 -0500

    Se intercambio el color verde pastel

--- src/helpers/styleData.js
+++ src/helpers/styleData.js
@@ -1,6 +1,7 @@
 export const STYLE_DATA_SETS = {
   background: {
-    pliego: 'rgb(4, 167, 119, .3)',
+    // pliego: 'rgb(4, 167, 119, .3)',
+    pliego: 'rgb(148, 229, 181, .3)',
     otros: 'rgb(249, 226, 175, .3)',
     inflacion: 'rgb(255, 0, 84, .3)'
   },
