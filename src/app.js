01f910ddd78a519388ee42be4c77a5e61dfc674a
BJChavez <joelchirit96@gmail.com>
2023-04-28 12:58:42 -0500

    Se soluciono el problema de los estilos en el canvas

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { dataPresidents } from '../data/db.js'
 import { changeTypeChart } from './events/event.js'
 
 const containerGeneral = document.querySelector('#chart')
+// const CANVAS_CLASS = ['lg:m-20', 'md:m-10', 'm-2', 'border-2', 'border-dashed', 'border-borde', 'rounded-lg', 'p-3', 'bg-canva']
 
 function createChart () {
   dataPresidents.map(data => {
