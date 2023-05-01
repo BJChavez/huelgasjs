32c8975526b46f8a7316aa2459d9c68875ec8988
BJChavez <joelchirit96@gmail.com>
2023-05-01 16:16:52 -0500

    Se cambio el path de la llamada a db.js

--- src/download/script.csv.js
+++ src/download/script.csv.js
@@ -1,5 +1,5 @@
 import jsonToCsvExport from 'https://cdn.jsdelivr.net/npm/json-to-csv-export@2.1.1/+esm'
-import { allData } from '../../data/allDb.js'
+import { allData } from '../data/allDb.js'
 
 const convert = {
   data: allData,
