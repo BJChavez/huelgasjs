b6178ef4fc637d10133b4543520f4c6b46ba0f64
BJChavez <joelchirit96@gmail.com>
2023-05-04 16:34:34 -0500

    Comentario a la funcion createChart que pinta los graficos

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const containerGeneral = document.querySelector('#chart')
 const dw = document.querySelector('#download')
 // const CANVAS_CLASS = ['lg:m-20', 'md:m-10', 'm-2', 'border-2', 'border-dashed', 'border-borde', 'rounded-lg', 'p-3', 'bg-canva']
 
+/*
+Esta funcion tiene la mision de recorrer cada interfaz del objeto char y añadirle los datos de db.js
+*/
 function createChart () {
   dataPresidents.map(data => {
     const newCanvas = document.createElement('canvas')
