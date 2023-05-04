a8400966d7d3029428f34117557a686b01b9c95e
BJChavez <joelchirit96@gmail.com>
2023-05-04 15:44:46 -0500

    Exportando constantes por separado

--- src/helpers/labelData.js
+++ src/helpers/labelData.js
@@ -1,5 +1,7 @@
-export const LABEL_CHART = {
+const LABEL_CHART = {
   pliego: 'Por pliego de reclamos',
   otros: 'Por otras causas',
   inflacion: 'Inflación'
 }
+
+export const { pliego, otros, inflacion } = LABEL_CHART
