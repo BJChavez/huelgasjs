9510b33a38c4f7d35baa3cd83e6c12e6bfb947bd
BJChavez <joelchirit96@gmail.com>
2023-05-04 16:37:55 -0500

    Doc: Se comento el por que esta logica esta separada de las demas

--- src/download/script.csv.js
+++ src/download/script.csv.js
@@ -1,6 +1,9 @@
 import csvDownload from 'json-to-csv-export'
 import { allData } from '../data/allDb.js'
 
+/*
+Esta logica esta separada por que es una funcionalidad de convertir y transformar un archivo .json
+*/
 const convert = {
   data: allData,
   filename: 'dataset',
