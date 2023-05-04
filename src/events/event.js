48f619dcc3ad1a7c006f6ff87d2c549f93e05591
BJChavez <joelchirit96@gmail.com>
2023-05-04 15:46:34 -0500

    Dsestructuramos el tipo de chart al incinio del codigo

--- src/events/event.js
+++ src/events/event.js
@@ -4,15 +4,17 @@ const TYPE_CHART = {
   line: 'line'
 }
 
+const { bar, line } = TYPE_CHART
+
 const graphic = document.querySelector('#graphics')
 
 export function changeTypeChart (ctx) {
   graphic.addEventListener('change', (option) => {
-    if (option.target.value === TYPE_CHART.bar) {
-      ctx.config.type = TYPE_CHART.bar
+    if (option.target.value === bar) {
+      ctx.config.type = bar
       ctx.update()
-    } else if (option.target.value === TYPE_CHART.line) {
-      ctx.config.type = TYPE_CHART.line
+    } else if (option.target.value === line) {
+      ctx.config.type = line
       ctx.update()
     }
   })
