39592e215cfa663a452153fa1d924d755938da32
BJChavez <joelchirit96@gmail.com>
2023-05-06 17:49:38 -0500

    Configurando la funcion para descargar los graficos en pdf

--- src/download/chart.pdf.js
+++ src/download/chart.pdf.js
@@ -1,2 +1,10 @@
-function downloadChart () {
+import JSpdf from 'jspdf'
+
+const pdf = new JSpdf('a3')
+
+export function chartPdf (data) {
+  const ig = new Image()
+  ig.src = data.toDataURL('image/png')
+  pdf.addImage(ig, 'JPEG', 25, 30, 250, 100)
+  // pdf.addImage(canvasImage, 'PNG', 10, 10, 150, 80)
 }
