d75a80e98304d6302adf38bf8476d00f3cb51154
BJChavez <joelchirit96@gmail.com>
2023-05-12 16:40:28 -0500

    Estilos redefinidos y mejor adaptados

--- src/helpers/styleCanvas.js
+++ src/helpers/styleCanvas.js
@@ -1,11 +1,9 @@
 export const CANVAS_CLASS = [
-  'lg:m-20',
+  'lg:m-15',
+  'border-dashed',
   'md:m-10',
   'm-2',
-  'border-2',
-  'border-dashed',
-  'border-borde',
   'rounded-lg',
-  'p-3',
-  'bg-canva'
+  'border-2',
+  'p-2'
 ]
