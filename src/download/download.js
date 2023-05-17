518818f8cdadd74446fd5ce4ffc9df82651b7988
BJChavez <joelchirit96@gmail.com>
2023-05-17 00:42:09 -0500

    La funcion para descargar pdf ha sido removida

--- src/download/download.js
+++ src/download/download.js
@@ -1,17 +1,7 @@
 import csvDownload from 'json-to-csv-export'
 import { allData } from '../../data/allDb.js'
-import JSpdf from 'jspdf'
 
 class Download {
-  chartPdf (data) {
-    const pdf = new JSpdf()
-    const ig = new Image()
-    ig.src = data.toDataURL('image/jpeg', 1.0)
-    pdf.setFontSize(20)
-    pdf.addImage(ig, 'PNG', 15, 15, 280, 150)
-    // pdf.save('ssss.pdf')
-  }
-
   scriptCsv (btn) {
     const convert = {
       data: allData,
